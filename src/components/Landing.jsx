733812ad48d17d2c437c460e32650026ebfc6f03
Ujwal Mamidi s3844467 <s3844467@student.rmit.edu.au>
2022-12-09 09:45:13 +1100

    working on landing + project page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
+import { BsFillArrowDownCircleFill } from 'react-icons/bs';
+import { AiFillGithub } from 'react-icons/ai';
+import { AiFillMail } from 'react-icons/ai';
+import { AiFillLinkedin } from 'react-icons/ai';
 
+import portfolioImage from '../assets/professional_uji_preview_rev_1.png';
 import '../styles/Landing.css'
 
 export default function Landing() {
   return (
     <div className='landing-container' id='landing'>
-      
+      {/* <div className='landing-container__left'>
+        <div className="img-container">
+          <img src={portfolioImage} alt="portfolio" />
+        </div>
+      </div>
+      <div className='landing-container__right'>
+        <div className="right-container">
+          <h2>hi, this is <span id="name">uji.</span></h2>
+          <h5>welcome to my <span id="emphasis">world.</span></h5>
+          <h6>A Software Engineering student from <span>Melbourne. </span> 
+          Has a strong passion in frontend development
+          and is an aspiring full stack developer.
+          </h6>
+          <a href="#about">
+          <BsFillArrowDownCircleFill className='landing-arrow'/>
+          </a>
+        </div>
+      </div> */}
     </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,15 +1,48 @@
-import React, { Component } from 'react'
+import React from 'react'
 
+import bookerooImg from '../assets/bookeroo.png';
+import { AiFillGithub } from 'react-icons/ai';
 import '../styles/Projects.css'
 
-export class Projects extends Component {
-  render() {
-    return (
-      <div className='projects-container' id='projects'>
+export default function Projects() {
+  // const portfolio = [
+  //   {
+  //     title: 'Bookeroo',
+  //     imgUrl: '',
+  //     stack: ['React.js', 'HTML/CSS', 'Java/Springboot', 'SQL'],
+  //     link: 'https://github.com/s3844467/JUVKS'
+  //   },
+  //   {
+  //     title: 'Path Planning Algorithm',
+  //     imgUrl: '',
+  //     stack: ['C++'],
+  //     link: 'https://github.com/s3844467/Path-Planning-Algorithm'
+  //   },
+  //   {
+  //     title: 'BlackJack',
+  //     imgUrl: '',
+  //     stack: ['Java', 'OOP', 'Swing/MVC'],
+  //     link: 'https://github.com/s3844467/Path-Planning-Algorithm'
+  //   }
+  // ]
 
+  return (
+    <div className='projects-container' id='projects'>
+      <div className="portfolios">
+        <div className="portfolio-item">
+          <div className="portfolio-image">
+            <img src={bookerooImg} alt="bookeroo" />
+          </div>
+          <div className="portfolio-image__hover">
+            <h3>Project Source</h3>
+            <div className="portfolio-image__icons">
+              <a href="www.google.com"><AiFillGithub className='icon'/></a>
+              {/* <a href="www.google"></a>
+              <a href="www.google"></a> */}
+            </div>
+          </div>
+        </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default Projects
